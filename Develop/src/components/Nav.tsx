335a567c324403bfaf08213c41fc6e9fd00df6af
JakeV64 <jakevonah@gmail.com>
2025-04-17 17:08:43 -0500

    Canidates components/interface

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,24 +1,31 @@
 import { Link } from "react-router-dom";
 
-
 const Nav = () => {
-
   // TODO: Add necessary code to display the navigation bar and link between the pages
+  const currentPage = useLocation().pathname;
   return (
-    
-    <nav className="nav">
-    <ul>
-      <li className="nav-item">
-        <Link className="nav-link" to="/">Home</Link>
+    <ul className='nav nav-tabs'>
+      <li className='nav-item'>
+        <Link
+          to='/'
+          className={currentPage === '/' ? 'nav-link active' : 'nav-link'}
+        >
+          Home
+        </Link>
       </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/candidates">Potential Candidates</Link>
+      <li className='nav-item'>
+        <Link
+          to='/SavedCandidates'
+          className={
+            currentPage === '/SavedCandidates' ? 'nav-link active' : 'nav-link'
+          }
+        >
+          Potential Candidates
+        </Link>
       </li>
     </ul>
-  </nav>
-   
-  
-  );
+  )
 };
+
 
 export default Nav;
--- Develop/src/pages/CandidateSearch.tsx
+++ Develop/src/pages/CandidateSearch.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import Candidate from '../interfaces/Candidate.interface';
+
 
 const CandidateSearch = () => {
   return <h1>CandidateSearch</h1>;
